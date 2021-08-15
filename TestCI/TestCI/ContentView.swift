ec855fdc7960bf462341fb8ad84378279d905dbc
Neha Pant <nehpant@gmail.com>
2021-08-15 21:48:04 +0100

    abc

---
+++ TestCI/TestCI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCI
+//
+//  Created by Neha Pant on 15/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
