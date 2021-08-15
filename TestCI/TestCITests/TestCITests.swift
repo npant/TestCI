9617d014cdd903f8d57a3d8c65916dcd41a83227
Neha Pant <nehpant@gmail.com>
2021-08-15 21:59:17 +0100

    Test

--- TestCI/TestCITests/TestCITests.swift
+++ TestCI/TestCITests/TestCITests.swift
@@ -21,6 +21,10 @@ class TestCITests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let a = 1
+        let b = 2
+        XCTAssert(a==b)
     }
 
     func testPerformanceExample() throws {
